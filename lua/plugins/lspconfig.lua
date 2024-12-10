9afc2bbfa9865941c61ce77700c23f448c591e95
Constanze <mezocsongor@gmail.com>
2024-12-10 02:13:51 +0100

    feat: python support

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -7,9 +7,13 @@ return {
     },
     config = function() 
         local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
         local lspconfig = require("lspconfig")
+
         lspconfig.rust_analyzer.setup({
+            capabilities = capabilities,
+        })
+
+        lspconfig.pyright.setup({
             capabilities = capabilities,
         })
     end
