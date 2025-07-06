06710c195bd35a5485c7cabd9a2691d6a2fee008
Constanze <mezocsongor@gmail.com>
2025-07-06 15:20:20 +0200

    add love to globals in love2d.lua

--- lua/plugins/love2d.lua
+++ lua/plugins/love2d.lua
@@ -1,6 +1,18 @@
 return {
     "S1M0N38/love2d.nvim",
     event = "VeryLazy",
-    opts = {},
+    config = function()
+        require("love2d").setup()
+
+        vim.lsp.config("lua_ls", {
+            settings = {
+                Lua = {
+                    diagnostics = {
+                        globals = { "love" },
+                    },
+                },
+            },
+        })
+    end,
     enabled = not require("custom/in_nvim").check(),
 }
