0a8713f374039e74be69fe4c19de396ac7868ab8
Constanze <mezocsongor@gmail.com>
2025-07-06 13:34:01 +0200

    better format for custom/in_nvim

--- lua/custom/in_nvim.lua
+++ lua/custom/in_nvim.lua
@@ -1,28 +1,30 @@
-return {
-    -- Checks whether the current working directory is the nvim directory or
-    -- a descendant of the nvim directory with depth at most 5.
-    check = function()
-        local max_depth = 5
+local M = {}
 
-        local path = vim.fn.getcwd()
-        local depth = 0
+--- Checks whether the current working directory is the nvim directory or
+--- a descendant of the nvim directory with depth at most 5.
+function M.check()
+    local max_depth = 5
 
-        while depth < max_depth do
-            local dir = vim.fn.fnamemodify(path, ":t")
-            if dir == "nvim" then
-                return true
-            end
+    local path = vim.fn.getcwd()
+    local depth = 0
 
-            local parent = vim.fn.fnamemodify(path, ":h")
-            if parent == path then
-                -- reached root directory
-                break
-            end
-            path = parent
-
-            depth = depth + 1
+    while depth < max_depth do
+        local dir = vim.fn.fnamemodify(path, ":t")
+        if dir == "nvim" then
+            return true
         end
 
-        return false
-    end,
-}
+        local parent = vim.fn.fnamemodify(path, ":h")
+        if parent == path then
+            -- reached root directory
+            break
+        end
+        path = parent
+
+        depth = depth + 1
+    end
+
+    return false
+end
+
+return M
