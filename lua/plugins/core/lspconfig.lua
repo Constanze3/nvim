8c2d4517cc93ea9914c9431ff30837a94822d259
Constanze <mezocsongor@gmail.com>
2025-07-10 08:30:15 +0200

    grugfar and hopefully vue support

--- lua/plugins/core/lspconfig.lua
+++ lua/plugins/core/lspconfig.lua
@@ -24,28 +24,60 @@ return {
         -- enable the language servers
         vim.lsp.enable(env.language_servers)
 
-        -- if both vue_ls and ts_ls are enabled configure ts_ls to support vue
-        if util.contains_multiple(env.language_servers, { "vue_ls", "ts_ls" }) then
-            local vue_plugin_location = vim.fn.stdpath("data")
-                .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+        -- vue_ls + vtsls config
+        -- based on https://github.com/vuejs/language-tools/wiki/Neovim
 
-            vim.lsp.config("ts_ls", {
-                init_options = {
-                    plugins = {
-                        {
-                            name = "@vue/typescript-plugin",
-                            location = vue_plugin_location,
-                            languages = { "javascript", "typescript", "vue" },
+        local vue_ls_path = vim.fn.stdpath("data")
+            .. "/mason/packages/vue-language-server/node_modules/@vue/language-server"
+
+        local vue_plugin = vim.lsp.config("vtsls", {
+            settings = {
+                vtsls = {
+                    tsserver = {
+                        globalPlugins = {
+                            {
+                                name = "@vue/typescript-plugin",
+                                location = vue_ls_path,
+                                languages = { "vue" },
+                                configNamespace = "typescript",
+                            },
                         },
                     },
                 },
-                filetypes = {
-                    "javascript",
-                    "typescript",
-                    "vue",
-                },
-            })
-        end
+            },
+            filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+        })
+
+        vim.lsp.config("vue_ls", {
+            on_init = function(client)
+                client.handlers["tsserver/request"] = function(_, result, context)
+                    local clients = vim.lsp.get_clients({ bufnr = context.bufnr, name = "vtsls" })
+                    if #clients == 0 then
+                        vim.notify(
+                            "Could not found `vtsls` lsp client, vue_lsp will not work without it.",
+                            vim.log.levels.ERROR
+                        )
+                        return
+                    end
+                    local ts_client = clients[1]
+
+                    local param = unpack(result)
+                    local id, command, payload = unpack(param)
+                    ts_client:exec_cmd({
+                        title = "vue_request_forward", -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+                        command = "typescript.tsserverRequest",
+                        arguments = {
+                            command,
+                            payload,
+                        },
+                    }, { bufnr = context.bufnr }, function(_, r)
+                        local response_data = { { id, r.body } }
+                        ---@diagnostic disable-next-line: param-type-mismatch
+                        client:notify("tsserver/response", response_data)
+                    end)
+                end
+            end,
+        })
 
         vim.api.nvim_create_autocmd("LspAttach", {
             callback = function()
